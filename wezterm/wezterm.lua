257b76dbbdd3ae280f7ac981e1b0fbdb355b958d
Tarang Kuhikar <tarang.devstree@gmail.com>
2024-07-16 09:11:56 +0530

    Add workspace changing keybind to wezterm.lua.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
+local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
@@ -17,5 +18,17 @@ config.tab_bar_at_bottom = true
 config.enable_scroll_bar = false
 config.use_fancy_tab_bar = false
 config.window_padding = { left = 3, right = 3, top = 3, bottom = 3 }
+
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
+
+config.keys = {
+  {
+    key = '9',
+    mods = 'ALT',
+    action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },
+  },
+}
 
 return config
