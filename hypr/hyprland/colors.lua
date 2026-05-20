5353459fa294004bfc1d7a830a32f65834e634f4
Tarang Kuhikar <tarangkuhikar@gmail.com>
2026-05-21 00:45:04 +0530

    add hyprland rice of end4

---
+++ hypr/hyprland/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(47464877)",
+            inactive_border = "rgba(1b1b1d33)",
+        },
+    },
+    misc = {
+        background_color = "rgba(131315FF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba(c1c6d6AA) rgba(c1c6d677)",
+})
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -2,7 +2,5 @@ OS="$(uname)"
 if [[ "${OS}" == "Darwin" ]] then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ "${OS}" == "Linux" && "$(tty)" == "/dev/tty1" ]] then
-  if uwsm check may-start; then
-      exec uwsm start hyprland.desktop
-  fi
+  start-hyprland
 fi
