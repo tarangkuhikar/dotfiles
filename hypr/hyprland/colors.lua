b9b39c2a1dc30ce74073fd477f0ee8c98a556285
Tarang Kuhikar <tarangkuhikar@gmail.com>
2026-08-16 18:33:38 +0530

    update config

--- hypr/hyprland/colors.lua
+++ hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(47464877)",
-            inactive_border = "rgba(1b1b1d33)",
+            active_border   = "rgba(44464f77)",
+            inactive_border = "rgba(1a1b2033)",
         },
     },
     misc = {
-        background_color = "rgba(131315FF)",
+        background_color = "rgba(121318FF)",
     },
 })
 
-hl.window_rule({
+hl.window_rule({ -- not sure how to syntax "pin 1"
     match        = { pin = 1 },
-    border_color = "rgba(c1c6d6AA) rgba(c1c6d677)",
+    border_color = "rgba(afc6ffAA) rgba(afc6ff77)",
 })
